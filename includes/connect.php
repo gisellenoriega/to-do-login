a1620e6dd1c13c608ac1659fe92928e7eeb6d868
Giselle Noriega <gnoriega626@gmail.com>
2015-04-21 20:52:00 -0700

    milestone 5 & 5.1

--- includes/connect.php
+++ includes/connect.php
@@ -1,10 +1,10 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 
 if($mysqli->connect_error) {
 	die('Connect Error(' . $mysqli->connect_errno .')'
 		.$mysqli->connect_error);
-
 }
 else {
 	echo "Connection made";
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
 	<title>To Do List</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<link rel="stylesheet" type="text/css" href="">
 </head>
 <body>
 	<div class="wrap">
