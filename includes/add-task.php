95a48a1cce24626ec6b9ca609e6cf7bf3dc73f8a
Giselle Noriega <gnoriega626@gmail.com>
2015-04-21 21:42:31 -0700

    milestone 11

--- includes/add-task.php
+++ includes/add-task.php
@@ -9,5 +9,15 @@
 	$mysqli ->query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");
 
 	$query = "SELECT * FROM tasks WHERE task='$task' and date='date' and time='time' ";
+
+	if ($result = $mysqli->query($query)) {
+		while ($row = $result->fetch_assoc()) {
+			$task_id = $row['id'];
+			$task_name = $row['task'];
+		}
+	}
+
+	$mysqli->close();
+
 	
 ?>
