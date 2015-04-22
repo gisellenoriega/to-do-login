bc1339afd4aff7d77850d84cc0b17f99d118ddeb
Giselle Noriega <gnoriega626@gmail.com>
2015-04-21 21:06:31 -0700

    milestone 7

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
 				<?php require("includes/connect.php"); ?>
 			</ul>
 		</div>
-		<form class="add_new-task" autocomplete="off">
-			<input type="text" name="new-task" placeholder="Add new item..."/>
-		</form>
-		</div>
+	<form class="add_new-task" autocomplete="off">
+		<input type="text" name="new-task" placeholder="Add new item..."/>
+	</form>
+	</div>
 </body>
 </html>
